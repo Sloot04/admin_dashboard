32401592cfd27a3c28aee2bf4edb3b58654a81dd
Suyai Slootmans <suyaislootmans@gmail.com>
2022-06-15 10:05:08 -0300

    UsersView

---
+++ lib/datatables/users_datasource.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class UsersDTS extends DataTableSource {
+  @override
+  DataRow getRow(int index) {
+    return DataRow.byIndex(index: index, cells: [
+      DataCell(Text('Avatar $index')),
+      DataCell(Text('Nombre $index')),
+      DataCell(Text('Email $index')),
+      DataCell(Text('UID $index')),
+      DataCell(Text('Acciones $index')),
+    ]);
+  }
+
+  @override
+  bool get isRowCountApproximate => false;
+
+  @override
+  int get rowCount => 100;
+
+  @override
+  int get selectedRowCount => 0;
+}
--- lib/ui/views/users_view.dart
+++ lib/ui/views/users_view.dart
@@ -1,22 +1,30 @@
 import 'package:flutter/material.dart';
-import 'package:admin_dashboard/ui/labels/custom_labels.dart';
 
-import '../cards/white_card.dart';
+import 'package:admin_dashboard/datatables/users_datasource.dart';
+
+import 'package:admin_dashboard/ui/labels/custom_labels.dart';
 
 class UsersView extends StatelessWidget {
   const UsersView({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final usersDataSource = UsersDTS();
     return Container(
-       padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+      padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
       child: ListView(
         physics: const ClampingScrollPhysics(),
         children: [
           Text('Users View', style: CustomLabels.h1),
           const SizedBox(height: 10),
-          const WhiteCard(
-              title: 'Sales Statistics', child:  Text('Hola Mundno')),
+          PaginatedDataTable(columns: const [
+            DataColumn(label: Text('Avatar')),
+            DataColumn(label: Text('Nombre')),
+            DataColumn(label: Text('Email')),
+            DataColumn(label: Text('UID')),
+            DataColumn(label: Text('Acciones')),
+          ], source: usersDataSource,
+          )
         ],
       ),
     );
